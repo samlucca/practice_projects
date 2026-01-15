fa5b8e62bf5665f1fe1155aa22f107acdb7db13d
Samuel <Samuel@Cygnus.ho.opspl.com>
2026-01-15 15:13:16 +0530

    added directory options

---
+++ practice-problems/DictionaryOperations.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Rahul",
+    "age": 21,
+    "course": "MCA"
+}
+
+student["college"]="abc college"
+student["age"]=22
+student.pop("course")
+
+print(student.keys())
+print(student.values())
